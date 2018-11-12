d36ee448354589715e746a90f7a7d1b957a1f61e
Yevhen Datsenko <gk1983@ukr.net>
2018-11-12 17:52:50 +0200

    create(@ID, @Name)

--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/AttributeClass.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/AttributeClass.java
@@ -1,11 +1,9 @@
 package com.ilopX.teach.java._02_SimpleReflection;
 
 public class AttributeClass {
-    private String name;
-    private int id;
 
     public AttributeClass(int id, String name) {
-        this.name = name;
-        this.id = id;
+
     }
+
 }
--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
@@ -1,4 +1,9 @@
 package com.ilopX.teach.java._02_SimpleReflection;
 
+import java.lang.annotation.*;
+
+@Target(value= ElementType.FIELD)
+@Retention(value= RetentionPolicy.RUNTIME)
 public @interface Name {
+
 }
