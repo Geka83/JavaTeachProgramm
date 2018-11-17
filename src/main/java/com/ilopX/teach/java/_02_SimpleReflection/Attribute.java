3696d4653a1dec789df82d5ee1ac2d0ff39feb11
Yevhen Datsenko <gk1983@ukr.net>
2018-11-17 19:08:58 +0200

    implement functional(to Attribute.java)

--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Attribute.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Attribute.java
@@ -1,21 +1,40 @@
 package com.ilopX.teach.java._02_SimpleReflection;
 
-import com.ilopX.teach.java.lib.NotImplementException;
+import java.lang.reflect.Field;
 
 public class Attribute {
 
-    public class FoundData {
+    public static class FoundData {
+        private int idValue;
+        private String nameValue;
 
+        public FoundData(int idValue, String nameValue){
+            this.idValue = idValue;
+            this.nameValue = nameValue;
+        }
         public int getIDValue() {
-            throw new NotImplementException();
+            return idValue;
         }
 
         public String getNameValue() {
-            throw new NotImplementException();
+            return nameValue;
         }
     }
 
-    public static FoundData Find(Object obj) {
-        throw new NotImplementException();
+    public static FoundData Find(Object obj) throws IllegalAccessException {
+        int idValue = 0;
+        String nameValue = null;
+        Class clazz = obj.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Name.class)) {
+                nameValue = (String) field.get(obj);
+            }
+            else if (field.isAnnotationPresent(ID.class)) {
+                idValue = (Integer) field.get(obj);
+            }
+        }
+        return new FoundData(idValue, nameValue);
     }
 }
