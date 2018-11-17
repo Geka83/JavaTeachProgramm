4f7d229687c92a6773133e31b299b592f608a9fb
Yevhen Datsenko <gk1983@ukr.net>
2018-11-17 21:32:47 +0200

    add parameters(@Name(minNameLength, maxNameLength))

--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
@@ -3,7 +3,9 @@ package com.ilopX.teach.java._02_SimpleReflection;
 import java.lang.annotation.*;
 
 @Inherited
-@Target(value= ElementType.FIELD)
-@Retention(value= RetentionPolicy.RUNTIME)
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface Name {
+    int minNameLength() default 3;
+    int maxNameLength() default 8;
 }
