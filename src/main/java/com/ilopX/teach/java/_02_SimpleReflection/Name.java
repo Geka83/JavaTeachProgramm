c976759ac9e455af15879fcaf4c94c70286ecae2
Yevhen Datsenko <gk1983@ukr.net>
2018-11-20 21:56:21 +0200

    (02/Name.java) add parameters

--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/Name.java
@@ -6,6 +6,7 @@ import java.lang.annotation.*;
 @Target(value = ElementType.FIELD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Name {
+
     int minNameLength() default 3;
     int maxNameLength() default 8;
 }
