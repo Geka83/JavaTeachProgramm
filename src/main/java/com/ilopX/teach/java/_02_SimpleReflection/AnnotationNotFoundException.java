579542a1e6f42fb63a4b313060ca7182c760ed62
Yevhen Datsenko <gk1983@ukr.net>
2018-12-08 17:58:53 +0200

    refactor_5(02/AnnotationNotFoundException.java): remove sout

--- src/main/java/com/ilopX/teach/java/_02_SimpleReflection/AnnotationNotFoundException.java
+++ src/main/java/com/ilopX/teach/java/_02_SimpleReflection/AnnotationNotFoundException.java
@@ -1,7 +1,4 @@
 package com.ilopX.teach.java._02_SimpleReflection;
 
 public class AnnotationNotFoundException extends Exception {
-    public AnnotationNotFoundException(String message) {
-        System.err.println(message);
-    }
 }
